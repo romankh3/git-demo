917859f6ae6de890fcbce2b025cc5342e3e2f2c5
Trunov Alexandr <trunovau@yandex.ru>
2022-06-03 21:53:28 +0300

    new class SomeClass new object test new method nums

--- SomeClass.java
+++ SomeClass.java
@@ -3,4 +3,12 @@ public class SomeClass {
     int x = 9;
     String s = "Some text";
 
+    SomeClass test = new SomeClass();
+
+    public void nums (){
+        System.out.println(x * 8);
+    }
+
+
+
 }
